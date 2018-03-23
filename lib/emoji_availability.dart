7fbe8d27f464db1b44ccbbb313aae637fc8342f7
Kevin Gray <KevinTheGray8@gmail.com>
2018-03-23 17:47:59 -0400

    type fix

--- lib/emoji_availability.dart
+++ lib/emoji_availability.dart
@@ -6,6 +6,7 @@ class EmojiAvailability {
   static const MethodChannel _channel =
       const MethodChannel('emoji_availability');
 
-  static Future<bool> checkEmojiAvailable(String emoji) =>
-      _channel.invokeMethod('checkEmojiAvailable', emoji);
+  static Future<bool> checkEmojiAvailable(String emoji) async {
+    return await _channel.invokeMethod('checkEmojiAvailable', emoji);
+  }
 }
